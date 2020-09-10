2a27e30af18c6fb707f95ba24637f85daff7f83e
Paulo Pieczarka <paulopieczarka@gmail.com>
2020-09-10 16:35:24 -0300

    Improve boleto success page

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,6 +3,7 @@ var config = {
         '*': {
             'ebanx': 'https://js.ebanxpay.com/ebanx-libjs-latest.min.js',
             'jquery_mask': 'https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js',
+            'jsbarcode': 'https://cdn.jsdelivr.net/npm/jsbarcode@3.11.0/dist/JsBarcode.all.min.js',
         }
     }
 };
